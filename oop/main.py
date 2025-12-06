9922c83a7d2496d758111ede096162f7b085fe7f
Emmanuel <blaemedia26@gmail.com>
2025-12-06 16:14:21 +0100

    Dive into Python Magic Methods

--- oop/main.py
+++ oop/main.py
@@ -1,5 +1,24 @@
 from library_system import Book, EBook, PrintBook, Library
+from book_class import Book
 
+def main():
+    # Creating an instance of Book
+    my_book = Book("1984", "George Orwell", 1949)
+
+    # Demonstrating the __str__ method
+    print(my_book)  # Expected to use __str__
+
+    # Demonstrating the __repr__ method
+    print(repr(my_book))  # Expected to use __repr__
+
+    # Deleting a book instance to trigger __del__
+    del my_book
+
+if __name__ == "__main__":
+    main()
+
+
+    
 def main():
     # Create a Library instance
     my_library = Library()
